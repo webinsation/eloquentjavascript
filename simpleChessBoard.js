2a2167b720929060b091975801c7848b2be805b1
Caleb Mellas <caleb@webinsation.com>
2015-01-26 13:36:23 -0600

    Create simpleChessBoard.js

---
+++ simpleChessBoard.js
@@ -0,0 +1,18 @@
+var width = 50;
+var height = 50;
+var white = " ";
+var black = "#";
+
+// controls the line breaks
+for (var h=0; h < height/2; h++) {
+    var row1 = "";  
+  	var row2 = "";
+  
+  	// controls chars on a line
+	for (var w=0; w < width/2; w++) {
+      row1 += (white + black);
+      row2 += (black + white);
+	}
+	console.log(row1);
+  	console.log(row2);
+}     
